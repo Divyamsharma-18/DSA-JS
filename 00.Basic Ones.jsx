7dae9b42f3300d0c9fb9b9e8b2eaa0cfd4679d13
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-04-10 17:02:21 +0530

    Add one question

--- 00.Basic Ones.jsx
+++ 00.Basic Ones.jsx
@@ -1,3 +1,6 @@
 // okay the basic one's that might be asked for fresher and in startups!
 
 // NUMBER OF WORDS IN STRING
+let str = "Hallo, ich bin Divyam."
+let arr = str.trim().split(/\s+/)
+console.log(arr.length);
