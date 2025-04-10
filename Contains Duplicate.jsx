dcfe51eaecd7b1dff454d82f71c4b2ab08ab04d3
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-04-10 16:24:18 +0530

    Add solution using SET

--- Contains Duplicate.jsx
+++ Contains Duplicate.jsx
@@ -1,3 +1,20 @@
 // CHECK IF ARRAY HAS A NUMBER APPEARED TWICE OR EACH ONE IS DISTINCT - [1,2,3,1] OR [1,2,3,4]
 
 // Using SET ( it only stores unique value )
+// Beats 97.28%
+
+function containsdouble(nums){
+    const seen = new Set();
+
+    for(let num of nums){
+        if(seen.has(num)){
+            return true;
+        }
+        seen.add(num);
+    }
+    return false;
+}
+
+console.log(containsdouble([1,2,3,1])); // true
+
+console.log(containsdouble([1,2,3,4])); // false
