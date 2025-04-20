0ff756e01d4dcf70f3a30b22eff059a264409c67
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-04-20 17:08:34 +0530

    Update 00.Basic Ones.jsx

--- 00.Basic Ones.jsx
+++ 00.Basic Ones.jsx
@@ -1,5 +1,17 @@
 // okay the basic one's that might be asked for fresher and in startups!
 
+
+// CHEAT Sheet -
+
+// str.trim("");                 remove space from start and end
+
+// str.split(/\s+/);             divides the string according to spaces
+
+// str.replace(/\s+/g, "")       removes all the space
+
+
+
+
 // NUMBER OF WORDS IN STRING
 let str = "Hallo, ich bin Divyam."
 let arr = str.trim().split(/\s+/)
